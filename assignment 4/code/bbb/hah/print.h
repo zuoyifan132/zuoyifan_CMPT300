296f49fee4d010b8cbb0c310d853496191bf3e1c
zuoyifan132 <zuoyifan132@gmail.com>
2020-08-09 16:23:25 -0700

    complete version of assignment 4

--- assignment 4/code/bbb/hah/print.h
+++
@@ -1,7 +0,0 @@
-#ifndef _PRINT_H_
-#define _PRINT_H_
-
-void print_no_option();
-void print_with_option(_Bool i, _Bool l, _Bool R, char* path);
-
-#endif
--- assignment 4/code/test2/stringProcess.h
+++
@@ -1,9 +0,0 @@
-#ifndef _STRINGPROCESS_H_
-#define _STRINGPROCESS_H_
-
-_Bool check_contain_char(char* s, char c);
-_Bool option_checker(char* s);
-_Bool filter(char* s);
-void sort_user_input(int argc, char** args); 
-
-#endif
